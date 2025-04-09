6fbe8f2b2265cd47142dbfaccdaac2178be64e22
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-09 17:44:17 -0300

    Create object with expected functions

---
+++ calculator.js
@@ -0,0 +1,8 @@
+export default function calculator() {
+  return {
+    add: function add() {},
+    subtract: function subtract() {},
+    divide: function divide() {},
+    multiply: function multiply() {},
+  };
+}
