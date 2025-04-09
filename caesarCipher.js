1a470801f5f30185e2f108a679f5dd3d6a629ff4
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-09 19:40:44 -0300

    Wrap alphabetical values and ignore simbols.
    Wrap alphabetical characters when value after shift goes over its limit
    (letter change case or converts to simbol) to preserve case and letter
    status, eg: x shift 2 -> a
    Ignore simbols mean ignore shift value when shifting, so the character
    doesn't change after shifting.

--- caesarCipher.js
+++ caesarCipher.js
@@ -7,9 +7,20 @@ export default caesarCipher = (string, shift) => {
 
   for (let i = 0; i < string.length; i++) {
     const charCode = string.charCodeAt(i);
-    const newCode = charCode + shift;
+    const newCode = shiftChar(charCode, shift);
     resArr.push(newCode);
   }
 
   return String.fromCharCode(...resArr);
 };
+
+function shiftChar(charCode, shift) {
+  if (charCode >= 65 && charCode <= 90) {
+    return ((charCode - 65 + shift) % 26) + 65;
+  }
+  if (charCode >= 97 && charCode <= 122) {
+    return ((charCode - 97 + shift) % 26) + 97;
+  }
+
+  return charCode;
+}
