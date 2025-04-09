df1054d9ed1d83878c457bd4a5f5d98f959d1f30
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-09 17:16:00 -0300

    Implement complete capitalize function

--- capitalize.js
+++ capitalize.js
@@ -1,3 +1,5 @@
 export default function capitalize(string) {
-  return string.toUpperCase();
+  const firstLetter = string.substring(0, 1);
+  const rest = string.substring(1);
+  return firstLetter.toUpperCase() + rest;
 }
