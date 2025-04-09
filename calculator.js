325f6eaf6957ef14710450aada8bec105660c107
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-09 17:51:48 -0300

    Implement add() function

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
 export default function calculator() {
   return {
-    add: function add() {},
+    add: function add(a, b) {
+      return a + b;
+    },
     subtract: function subtract() {},
     divide: function divide() {},
     multiply: function multiply() {},
