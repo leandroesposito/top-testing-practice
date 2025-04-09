073007a8ae9360997eb3bf9921e113b5aacf0222
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-09 20:28:46 -0300

    Implement average

--- analizeArray.js
+++ analizeArray.js
@@ -1,3 +1,11 @@
 export default function analizeArray(array) {
-  return {};
+  const length = array.length;
+
+  const sum = array.reduce((prev, curr) => curr + prev, 0);
+
+  const average = sum / length;
+
+  return {
+    average,
+  };
 }
