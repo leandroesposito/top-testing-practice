89f343329a049eedf0c35eac923e3e519d2a8030
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-09 20:31:33 -0300

    Implement max and length

--- analizeArray.js
+++ analizeArray.js
@@ -6,9 +6,12 @@ export default function analizeArray(array) {
   const average = sum / length;
 
   const min = Math.min(...array);
+  const max = Math.max(...array);
 
   return {
     average,
     min,
+    max,
+    length,
   };
 }
