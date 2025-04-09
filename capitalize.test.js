132fbbf9619b37d3d116b52effb0555b8754051a
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-09 17:15:16 -0300

    Add more test cases

--- capitalize.test.js
+++ capitalize.test.js
@@ -23,6 +23,46 @@ test("capitalize return capitalized string", () => {
       value: ".",
       result: ".",
     },
+    {
+      value: "aa",
+      result: "Aa",
+    },
+    {
+      value: ".a",
+      result: ".a",
+    },
+    {
+      value: ".a 1 2 3",
+      result: ".a 1 2 3",
+    },
+    {
+      value: "a 1 2 3",
+      result: "A 1 2 3",
+    },
+    {
+      value: "a 1 2 a",
+      result: "A 1 2 a",
+    },
+    {
+      value: "word",
+      result: "Word",
+    },
+    {
+      value: "hello world!",
+      result: "Hello world!",
+    },
+    {
+      value: "123",
+      result: "123",
+    },
+    {
+      value: "a123",
+      result: "A123",
+    },
+    {
+      value: "123a",
+      result: "123a",
+    },
   ];
 
   tests.forEach((test) => {
